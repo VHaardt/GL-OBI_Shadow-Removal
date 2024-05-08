b812932432025dff7d41705f0bb9c819ced66e46
Vittorio Haardt <vittoriohaardt@gmail.com>
2024-05-08 12:12:28 +0200

    Update exposure.py

--- utils/exposure.py
+++ utils/exposure.py
@@ -5,6 +5,20 @@ def exposureRGB(img, R_a, R_b, G_a, G_b, B_a, B_b):
     rgb_img[:, :, 0] = (rgb_img[:, :, 0]*R_a) + R_b
     rgb_img[:, :, 1] = (rgb_img[:, :, 1]*G_a) + G_b
     rgb_img[:, :, 2] = (rgb_img[:, :, 2]*B_a) + B_b
+    
+    rgb_img = np.clip(rgb_img, 0, 1)
+    
+    return rgb_img
+
+def exposureRGB_Tens(img, R_a, R_b, G_a, G_b, B_a, B_b):
+    rgb_img = img.clone()  # Cloning to prevent modification of the original tensor
+    
+    # Applying the exposure transformation to each channel
+    rgb_img[:, 0, :, :] = (rgb_img[:, 0, :, :] * R_a) + R_b
+    rgb_img[:, 1, :, :] = (rgb_img[:, 1, :, :] * G_a) + G_b
+    rgb_img[:, 2, :, :] = (rgb_img[:, 2, :, :] * B_a) + B_b
+    
+    # Clipping the values to maintain the range [0, 1]
     rgb_img = torch.clamp(rgb_img, 0, 1)
     
     return rgb_img
