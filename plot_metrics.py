809f64b3c4524930f0834f84305d1c09609d2615
Vittorio Haardt <vittoriohaardt@gmail.com>
2024-05-17 16:16:31 +0200

    Add files via upload

---
+++ plot_metrics.py
@@ -0,0 +1,49 @@
+#new 2
+import matplotlib.pyplot as plt
+import numpy as np
+import json
+import os
+import argparse
+import ipdb
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Plot metrics')
+    parser.add_argument('--path', type=str, help='Path to the metrics file)')
+    args = parser.parse_args()
+
+    if args.path is None:
+        experiments = os.listdir("checkpoints")
+        # sort experiments by date of creation
+        experiments.sort(key=lambda x: os.path.getctime(os.path.join("checkpoints", x)))
+        args.path = os.path.join("checkpoints", experiments[-1], "metrics.json")
+
+    with open(args.path, 'r') as file:
+        metrics = json.load(file)
+
+
+    metrics_names = []
+    plots = []
+    for metric, values in metrics.items():
+        split = metric.split("_")[0]
+        metric_name = metric.replace(split + "_", "")
+
+        if metric_name not in metrics_names:
+            metrics_names.append(metric_name)
+            # create plot
+            plt.figure()
+            plt.title(metric_name)
+            plt.xlabel('Epoch')
+            plt.ylabel(metric_name)
+
+        # plot
+        plt.plot(values, label=split)
+        plt.legend()
+
+    plt.show()
+
+
+
+        
+
+
+
