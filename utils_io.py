975d65612302864a3b30cad37c0987c22dc862c4
Vittorio Haardt <vittoriohaardt@gmail.com>
2024-05-17 16:20:41 +0200

    Delete utils_io.py

--- utils_io.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-def read_image(image_path, grayscale=False):
-    img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-
-    if not grayscale:
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = img.astype(np.float32) / 255.0
-    return img
